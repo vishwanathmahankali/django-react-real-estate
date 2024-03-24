2cd8680b1e6fdc8398c51d7f889c1785461184d8
vishwanathmahankali <vishwanathmahankali@gmail.com>
2024-03-24 21:14:39 +0530

    feat: user auth-register,login,password reset

--- real_estate/urls.py
+++ real_estate/urls.py
@@ -5,6 +5,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('estate/', admin.site.urls),
+    path("api/v1/auth/", include("djoser.urls")),
+    path("api/v1/auth/", include("djoser.urls.jwt")),
+    
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 admin.site.site_header = "Real Estate Admin"
