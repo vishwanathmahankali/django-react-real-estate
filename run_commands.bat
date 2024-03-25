284fae1f1ac21b7aa93a8f6296c6b975740fe0fc
vishwanathmahankali <vishwanathmahankali@gmail.com>
2024-03-25 23:40:20 +0530

    chore: setup makefile, run_command.bat file

---
+++ run_commands.bat
@@ -0,0 +1,83 @@
+@echo off
+
+if exist .env (
+    call .env
+)
+
+set ENV_FILE_PARAM=--env-file .env
+
+:menu
+echo.
+echo Select an option:
+echo 1. Build Docker containers
+echo 2. Start Docker containers
+echo 3. Stop Docker containers
+echo 4. Show Docker logs
+echo 5. Run database migrations
+echo 6. Create superuser
+echo 7. Collect static files
+echo 8. Remove Docker containers and volumes
+echo 9. Inspect Docker volume
+echo 10. Access PostgreSQL database
+echo 11. Run tests
+echo 12. Run tests and generate HTML coverage report
+echo 13. Run Flake8 linting
+echo 14. Check code formatting with Black
+echo 15. Show code formatting diff with Black
+echo 16. Format code with Black
+echo 17. Check import sorting with isort
+echo 18. Show import sorting diff with isort
+echo 19. Format imports with isort
+echo 20. Run makemigrations
+echo q. Quit
+echo.
+
+set /p choice=Enter your choice: 
+
+if "%choice%"=="1" (
+    docker-compose up --build -d --remove-orphans
+) else if "%choice%"=="2" (
+    docker-compose up -d
+) else if "%choice%"=="3" (
+    docker-compose down
+) else if "%choice%"=="4" (
+    docker-compose logs
+) else if "%choice%"=="5" (
+    docker-compose exec api python manage.py migrate
+) else if "%choice%"=="6" (
+    docker-compose exec api python manage.py createsuperuser
+) else if "%choice%"=="7" (
+    docker-compose exec api python manage.py collectstatic --no-input --clear
+) else if "%choice%"=="8" (
+    docker-compose down -v
+) else if "%choice%"=="9" (
+    docker volume inspect estate-src_postgres_data
+) else if "%choice%"=="10" (
+    docker-compose exec postgres-db psql --username=postgres --dbname=estate
+) else if "%choice%"=="11" (
+    docker-compose exec api pytest -p no:warnings --cov=.
+) else if "%choice%"=="12" (
+    docker-compose exec api pytest -p no:warnings --cov=. --cov-report=html
+) else if "%choice%"=="13" (
+    docker-compose exec api flake8 .
+) else if "%choice%"=="14" (
+    docker-compose exec api black --check --exclude=migrations .
+) else if "%choice%"=="15" (
+    docker-compose exec api black --diff --exclude=migrations .
+) else if "%choice%"=="16" (
+    docker-compose exec api black --exclude=migrations .
+) else if "%choice%"=="17" (
+    docker-compose exec api isort . --check-only --skip=env --skip=migrations
+) else if "%choice%"=="18" (
+    docker-compose exec api isort . --diff --skip=env --skip=migrations
+) else if "%choice%"=="19" (
+    docker-compose exec api isort . --skip=env --skip=migrations
+) else if "%choice%"=="20" (
+    docker-compose exec api python manage.py makemigrations
+) else if /i "%choice%"=="q" (
+    exit
+) else (
+    echo Invalid choice. Please try again.
+)
+
+goto menu
